f18f372c172d1521ac00a5c5569d76fe114fd8f1
Maxim Talancev <maxtalwork@gmail.com>
2024-05-30 02:38:05 +0300

    Прописали функции и методы

--- crs/class_db_manager.py
+++ crs/class_db_manager.py
@@ -1,34 +1,60 @@
+import psycopg2
+
+
 class DBManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, host, port, user, password):
+        self.connection = psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password)
 
     def get_companies_and_vacancies_count(self):
         """
         получает список всех компаний и количество вакансий у каждой компании.
         """
-        pass
+        query = 'select company, COUNT(*) from vacancies'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
     def get_all_vacancies(self):
         """
         получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
         """
-        pass
+        query = 'select * from vacancies'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
     def get_avg_salary(self):
         """
         получает среднюю зарплату по вакансиям.
         """
-        pass
+        query = 'select avg(salary) from vacancies'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
     def get_vacancies_with_higher_salary(self):
         """
         получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         """
-        pass
+        query = 'select * from vacancies where salary > (select avg(salary) from vacancies)'
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
 
     def get_vacancies_with_keyword(self):
         """
         получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
         """
-        pass
+        user_req = input('Введите ваш запрос: ').lower()
+        query = f"select * from vacancies where lower(title) like '%{user_req}%'"
+        with self.connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall()
+
+    def __exit__ (self, exc_type, exc_val, exc_tab):
+        self.connection.close()
--- main/functions.py
+++ main/functions.py
@@ -2,14 +2,58 @@ import requests
 import psycopg2
 
 
-companies = [991318, 2509634, 816, 3735331, 132654, 26250, 89117, 8893, 126025, 2242]
+def get_vacancy_data(companies):
+    for employer_id in companies:
+        params = {'employer_id': employer_id}
+        response = requests.get('https://api.hh.ru/vacancies', params=params)
+        for vacancy in response.json()['items']:
+            vacancy_id = vacancy['id']
+            comp_id = vacancy['employer']['id']
+            vac_name = vacancy['name']
+            try:
+                salary = vacancy['salary']['from']
+            except TypeError:
+                salary = 0
+            url = vacancy['url']
+            return f'{vacancy_id}, "{vac_name}", {salary}, {comp_id}, "{url}"'
 
-for employer_id in companies:
-    params = {'employer_id': employer_id}
-    response = requests.get('https://api.hh.ru/vacancies', params=params)
-    for vacancy in response.json()['items']:
-        print(vacancy['name'])
-    url_st = f'https://api.hh.ru/employers/{employer_id}'
-    response2 = requests.get(url_st, params=params)
-    print(f'\nComp')
-    print(f'{response2.json()['name']}\n')
+
+def get_comp_data(companies):
+    for employer_id in companies:
+        params = {'id': employer_id}
+        url_st = f'https://api.hh.ru/employers/{employer_id}'
+        response2 = requests.get(url_st, params=params)
+        comp_id = int(response2.json()['id'])
+        comp_name = response2.json()['name']
+        return f'{comp_id}, {comp_name}'
+
+
+def create_table_vac(host, port, user, password, companies):
+    with psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('create table if not exists vacancies(vac_id serial primary key, title varchar(100), '
+                           'salary int, comp_id int, url varchar(100))')
+            # cursor.execute(f"INSERT INTO vacancies VALUES ({get_vacancy_data(companies)})")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
+
+
+def create_table_comp(host, port, user, password, companies):
+    with psycopg2.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('create table if not exists companies (comp_id serial primary key, comp_title varchar(100))')
+            # cursor.execute(f"INSERT INTO companies VALUES ({get_comp_data(companies)})")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
